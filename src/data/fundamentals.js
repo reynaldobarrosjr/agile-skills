29be9853d68aa402c34385c8774e60e3646c2768
Reynaldo Barros Jr <413448+reynaldobarrosjr@users.noreply.github.com>
2020-05-03 17:47:51 -0300

    teste x1 vetor para aparecer

--- src/data/fundamentals.js
+++ src/data/fundamentals.js
@@ -64,7 +64,7 @@ export const fundamentalsCollection = {
 							},
 							skills: [
 								{
-									name:"Scrum Master",
+									name:"x1",
 									description:{
 										text:"teste",
 										links:[
